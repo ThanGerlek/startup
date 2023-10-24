fd8a78593a3fae43a6d3b5775bfd4e28ee0add20
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-10-24 02:35:24 -0700

    fix(js): import message-display and replace duplicated code

--- find-friend.js
+++ find-friend.js
@@ -1,7 +1,7 @@
 'use strict';
 
-//BUG empty string fails to connect
-
+import { setupWaitNotification, cancelWaitNotification, displayMessage, clearMessageDisplay } from "./message-display.js";
+ 
 function onSubmitButtonClick() {
     clearMessageDisplay();
     setupWaitNotification();
@@ -87,56 +87,5 @@ function openWebSocketConnection(response) {
     // TODO implement WebSocket
 }
 
-
-
-
-
-// DUPLICATED CODE: Message display code
-
-
-
-function setupWaitNotification() {
-    window.waitNotification = setTimeout(() => {
-        // TODO? Separate from other messages (don't want this to overwrite them)
-        displayMessage('info', 'Connecting to the server, please wait...');
-    }, 1000);
-    // BUG Wait notification doesn't display (it should pause for 1s, display for 1s, then redirect)
-}
-
-function cancelWaitNotification() {
-    if (window.waitNotification) {
-        clearTimeout(window.waitNotification);
-        delete window.waitNotification;
-    }
-}
-
-function displayMessage(msgType, msg) {
-    clearMessageDisplay();
-    let msgElement = getMessageElement(msgType);
-    msgElement.textContent = msg;
-    msgElement.style.display = 'inline';
-}
-
-function clearMessageDisplay() {
-    let messageTypes = ['info', 'warn', 'error'];
-    let messageElements = messageTypes.map((msgType) => getMessageElement(msgType));
-    messageElements.forEach((msgElement) => {
-        msgElement.textContent = '';
-        msgElement.style.display = 'none';
-    });
-}
-function getMessageElement(msgType) {
-    if (!(msgType === 'info' || msgType === 'warn' || msgType === 'error')) {
-        throw new Error(`Invalid message element type '${msgType}' (must be one of 'info', 'warn', 'error')`)
-    }
-
-    let elementId = msgType + '_message';
-    let msgElement = document.getElementById(elementId);
-    if (msgElement == null) {
-        throw new Error(`Couldn't find message element ${elementId}!`);
-    } else {
-        return msgElement;
-    }
-}
 
 document.getElementById('submit-game-request-button').addEventListener('click', () => onSubmitButtonClick());
