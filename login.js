070a2e20cd3c400a7dcf68254e37d62424c2ce2d
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-10-24 02:28:51 -0700

    fix(js): remove code duplicated by display-message import

--- login.js
+++ login.js
@@ -86,49 +86,6 @@ function loginUser(token) {
     window.location.href = 'home.html';
 }
 
-function setupWaitNotification() {
-    window.waitNotification = setTimeout(() => {
-        // TODO? Separate from other messages (don't want this to overwrite them)
-        displayMessage('info', 'Connecting to the server, please wait...');
-    }, 1000);
-}
-
-function cancelWaitNotification() {
-    if (window.waitNotification) {
-        clearTimeout(window.waitNotification);
-        delete window.waitNotification;
-    }
-}
-
-function displayMessage(msgType, msg) {
-    clearMessageDisplay();
-    let msgElement = getMessageElement(msgType);
-    msgElement.textContent = msg;
-    msgElement.style.display = 'inline';
-}
-
-function clearMessageDisplay() {
-    let messageTypes = ['info', 'warn', 'error'];
-    let messageElements = messageTypes.map((msgType) => getMessageElement(msgType));
-    messageElements.forEach((msgElement) => {
-        msgElement.textContent = '';
-        msgElement.style.display = 'none';
-    });
-}
-function getMessageElement(msgType) {
-    if (!(msgType === 'info' || msgType === 'warn' || msgType === 'error')) {
-        throw new Error(`Invalid message element type '${msgType}' (must be one of 'info', 'warn', 'error')`)
-    }
-
-    let elementId = msgType + '_message';
-    let msgElement = document.getElementById(elementId);
-    if (msgElement == null) {
-        throw new Error(`Couldn't find message element ${elementId}!`);
-    } else {
-        return msgElement;
-    }
-}
-
 function extractUsername() {
     return document.getElementById('usernameBox').value;
 }
