4492821f0e88c619fcde15c862fb929141b7b9dd
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-11-04 23:10:37 -0700

    feat(service): stub out index.js

--- index.js
+++ index.js
@@ -1,1 +1,86 @@
+'use strict'
+
+import {clearApplication} from "services/clearApplication.js";
+import {joinGame} from "services/joinGame.js";
+import {login} from "services/login.js";
+import {logout} from "services/logout.js";
+import {register} from "services/register.js";
+
+const express = require('express');
+const app = express();
+
+app.listen(5600);
+
+
+// Clear application
+app.delete('/db', (req, res, next) => {
+    // TODO(db)!
+    clearApplication();
+});
+// | **Request class**    | N/A (no request body)                                          |
+// | **Response class**   | MessageResponse                                                |
+// | **Description**      | Clears the database. Removes all users, games, and authTokens. |
+// | **Success response** | [200]                                                          |
+// | **Failure response** | [500] `{ "message": "Error: description" }`                    |
+
+
+// Register
+app.post('/user', (req, res, next) => {
+    // TODO!
+    register();
+});
+// | **Request class**    | RegisterRequest                               |
+// | **Response class**   | AuthResponse                                  |
+// | **Description**      | Register a new user.                          |
+// | **Body**             | `{ "username":"", "password":"" }`            |
+// | **Success response** | [200] `{ "username":"", "token":"" }`         |
+// | **Failure response** | [400] `{ "message": "Error: bad request" }`   |
+// | **Failure response** | [403] `{ "message": "Error: already taken" }` |
+// | **Failure response** | [500] `{ "message": "Error: description" }`   |
+
+
+//  Login
+app.post('/session', (req, res, next) => {
+    // TODO!
+    login();
+});
+// | **Request class**    | LoginRequest                                    |
+// | **Response class**   | AuthResponse                                    |
+// | **Description**      | Logs in an existing user (returns a new token). |
+// | **Body**             | `{ "username":"", "password":"" }`              |
+// | **Success response** | [200] `{ "username":"", "token":"" }`           |
+// | **Failure response** | [401] `{ "message": "Error: unauthorized" }`    |
+// | **Failure response** | [500] `{ "message": "Error: description" }`     |
+
+
+//  Logout
+app.delete('/session', (req, res, next) => {
+    // TODO!
+    logout();
+});
+// | **Request class**    | N/A (no request body)                        |
+// | **Response class**   | MessageResponse                              |
+// | **Description**      | Logs out the user represented by the token.  |
+// | **Headers**          | `authorization: <token>`                     |
+// | **Success response** | [200]                                        |
+// | **Failure response** | [401] `{ "message": "Error: unauthorized" }` |
+// | **Failure response** | [500] `{ "message": "Error: description" }`  |
+
+//  Join Game
+app.put('/game', (req, res, next) => {
+    // TODO!
+    joinGame();
+});
+// | **Request class**    | JoinGameRequest                                                                                                                                                                            |
+// | **Response class**   | MessageResponse                                                                                                                                                                            |
+// | **Description**      | If a game with the specified players exists, joins the game. Otherwise, if a game request exists, a new (empty) game is created and WS requests are sent. Otherwise, a request is created. |
+// | **Headers**          | `authorization: <authToken>`                                                                                                                                                               |
+// | **Body**             | `{ "player":"playerName", "opponent": "opponentName", "firstPlayer": "playerName" }`                                                                                                       |
+// | **Success response** | [200] `{ "message": "OK", "board": <boardState> }`                                                                                                                                         |
+// | **Failure response** | [400] `{ "message": "Error: bad request" }`                                                                                                                                                |
+// | **Failure response** | [401] `{ "message": "Error: unauthorized" }`                                                                                                                                               |
+// | **Failure response** | [403] `{ "message": "Error: already taken" }`                                                                                                                                              |
+// | **Failure response** | [500] `{ "message": "Error: description" }`                                                                                                                                                |
+
+
 document.addEventListener('DOMContentLoaded', () => authenticateToken(redirectToHomePage, redirectToLoginPage));
