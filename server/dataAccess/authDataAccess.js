b6bbda02183607eab67226a31e49563c7aa82db3
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-11-08 14:23:23 -0700

    enhance(dao): change to use classes

--- server/dataAccess/authDataAccess.js
+++ server/dataAccess/authDataAccess.js
@@ -3,41 +3,45 @@
 // Auth Data Access
 // TODO! db
 
-/**
- * Registers the given token as a valid token.
- *
- * @param token the AuthToken to register
- */
-function addToken(token) {
+let AuthDAO = class {
 
+    /**
+     * Registers the given token as a valid token.
+     *
+     * @param token the AuthToken to register
+     */
+    addToken(token) {
+        // TODO
+    }
+
+    /**
+     * Checks if the given token is currently valid.
+     *
+     * @param token the token to validate
+     * @return true iff the given token is currently valid
+     */
+    isValidToken(token) {
+        // TODO
+        return true;
+    }
+
+    /**
+     * Invalidates the given token. Future calls requiring authorization for the given user will need to
+     * generate a new token by re-authenticating.
+     *
+     * @param token the token to invalidate
+     */
+    removeToken(token) {
+        // TODO
+    }
+
+    /**
+     * Invalidates every currently valid token. Future calls requiring authorization will need to generate
+     * new tokens by re-authenticating.
+     */
+    clearTokens() {
+        // TODO
+    }
 }
 
-/**
- * Checks if the given token is currently valid.
- *
- * @param token the token to validate
- * @return true iff the given token is currently valid
- */
-function isValidToken(token) {
-    return true;
-}
-
-/**
- * Invalidates the given token. Future calls requiring authorization for the given user will need to
- * generate a new token by re-authenticating.
- *
- * @param token the token to invalidate
- */
-function removeToken(token) {
-
-}
-
-/**
- * Invalidates every currently valid token. Future calls requiring authorization will need to generate
- * new tokens by re-authenticating.
- */
-function clearTokens() {
-
-}
-
-export {addToken, isValidToken, removeToken, clearTokens};
+module.exports = AuthDAO;
--- server/dataAccess/gameDataAccess.js
+++ server/dataAccess/gameDataAccess.js
@@ -3,73 +3,81 @@
 // Game Data Access
 // TODO! db
 
-/**
- * Inserts a game into the database.
- * @param game the game to insert
- * @return the gameID of the inserted game
- */
-function insertGame(game) {
-    /* Failures
-    can't access database
-    */
+let GameDAO = class {
+    /**
+     * Inserts a game into the database.
+     * @param game the game to insert
+     * @return the gameID of the inserted game
+     */
+    insertGame(game) {
+        /* Failures
+        can't access database
+        */
+        // TODO
+    }
+
+    /**
+     * Returns the gameID of the game between the players if it exists, otherwise returns null
+     *
+     * @param player1
+     * @param player2
+     */
+    findGame(player1, player2) {
+        // TODO
+        return -1;
+    }
+
+    /**
+     * Fetches the game with the given ID from the database.
+     *
+     * @param gameID the ID of the game to fetch
+     * @return the fetched game
+     */
+    getGameState(gameID) {
+        /* Failures
+        can't access database
+        game not found
+        */
+        // TODO
+        return null;
+    }
+
+    /**
+     * Updates the game state of a game in the database to match the given version.
+     *
+     * @param gameID the ID of the game to update
+     * @param game the updated version of the Game
+     */
+    updateGameState(gameID, game) {
+        /* Failures
+        can't access database
+        game not found
+        */
+        // TODO
+    }
+
+    /**
+     * Removes a single game from the database.
+     *
+     * @param gameID the ID of the Game to remove
+     */
+    removeGame(gameID) {
+        /* Failures
+        can't access database
+        game not found
+        */
+        // TODO
+    }
+
+    /**
+     * Removes every game from the database.
+     */
+    clearGames() {
+        /* Failures
+        can't access database
+        */
+        // TODO
+    }
 }
 
-/**
- * Returns the gameID of the game between the players if it exists, otherwise returns null
- *
- * @param player1
- * @param player2
- */
-function findGame(player1, player2) {
-    return -1;
-}
-
-/**
- * Fetches the game with the given ID from the database.
- *
- * @param gameID the ID of the game to fetch
- * @return the fetched game
- */
-function getGameState(gameID) {
-    /* Failures
-    can't access database
-    game not found
-    */
-    return null;
-}
-
-/**
- * Updates the game state of a game in the database to match the given version.
- *
- * @param gameID the ID of the game to update
- * @param game the updated version of the Game
- */
-function updateGameState(gameID, game) {
-    /* Failures
-    can't access database
-    game not found
-    */
-}
-
-/**
- * Removes a single game from the database.
- *
- * @param gameID the ID of the Game to remove
- */
-function removeGame(gameID) {
-    /* Failures
-    can't access database
-    game not found
-    */
-}
-
-/**
- * Removes every game from the database.
- */
-function clearGames() {
-    /* Failures
-    can't access database
-    */
-}
-
-export {insertGame, findGame, getGameState, updateGameState, removeGame, clearGames};
+module.exports = GameDAO;
