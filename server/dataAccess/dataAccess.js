a7fd8e5bedc9d7a3abd584a419bcbc9d7587eebc
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-11-08 20:58:47 -0700

    feat(dao): add custom Error types to dataAccess

--- server/dataAccess/dataAccess.js
+++ server/dataAccess/dataAccess.js
@@ -3,7 +3,7 @@ const GameDAO = require("./gameDataAccess");
 const GameRequestDAO = require("./gameRequestDataAccess");
 const UserDAO = require("./userDataAccess");
 
-module.exports.getNewDAOs = function() {
+module.exports.getNewDAOs = function () {
     return {
         authDAO: new AuthDAO(), gameDAO: new GameDAO(), gameRequestDAO: new GameRequestDAO(), userDAO: new UserDAO()
     };
@@ -13,3 +13,20 @@ module.exports.AuthDAO = AuthDAO;
 module.exports.GameDAO = GameDAO;
 module.exports.GameRequestDAO = GameRequestDAO;
 module.exports.UserDAO = UserDAO;
+
+class DataAccessError extends Error {
+}
+
+module.exports.DataAccessError = DataAccessError;
+
+module.exports.BadRequestError = class extends DataAccessError {
+};
+
+module.exports.NoSuchItemError = class extends DataAccessError {
+};
+
+module.exports.UnauthorizedAccessError = class extends DataAccessError {
+};
+
+module.exports.ValueAlreadyTakenError = class extends DataAccessError {
+};
