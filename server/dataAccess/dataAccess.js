f4a4bfc715509a4bb922ecfc0071f605c797214c
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-11-09 13:17:06 -0700

    feat(js): reorganized exports

--- server/dataAccess/dataAccess.js
+++ server/dataAccess/dataAccess.js
@@ -3,30 +3,36 @@ const GameDAO = require("./gameDataAccess");
 const GameRequestDAO = require("./gameRequestDataAccess");
 const UserDAO = require("./userDataAccess");
 
-module.exports.getNewDAOs = function () {
-    return {
-        authDAO: new AuthDAO(), gameDAO: new GameDAO(), gameRequestDAO: new GameRequestDAO(), userDAO: new UserDAO()
-    };
-};
-
-module.exports.AuthDAO = AuthDAO;
-module.exports.GameDAO = GameDAO;
-module.exports.GameRequestDAO = GameRequestDAO;
-module.exports.UserDAO = UserDAO;
 
 class DataAccessError extends Error {
 }
 
-module.exports.DataAccessError = DataAccessError;
+class BadRequestError extends DataAccessError {
+}
 
-module.exports.BadRequestError = class extends DataAccessError {
-};
+class NoSuchItemError extends DataAccessError {
+}
 
-module.exports.NoSuchItemError = class extends DataAccessError {
-};
+class UnauthorizedAccessError extends DataAccessError {
+}
 
-module.exports.UnauthorizedAccessError = class extends DataAccessError {
-};
+class ValueAlreadyTakenError extends DataAccessError {
+}
 
-module.exports.ValueAlreadyTakenError = class extends DataAccessError {
+
+module.exports = {
+    AuthDAO,
+    GameDAO,
+    GameRequestDAO,
+    UserDAO,
+    DataAccessError,
+    BadRequestError,
+    NoSuchItemError,
+    ValueAlreadyTakenError,
+    UnauthorizedAccessError,
+    getNewDAOs: function () {
+        return {
+            authDAO: new AuthDAO(), gameDAO: new GameDAO(), gameRequestDAO: new GameRequestDAO(), userDAO: new UserDAO()
+        };
+    }
 };
--- server/services/services.js
+++ server/services/services.js
@@ -6,24 +6,25 @@ const LoginService = require('./login');
 const LogoutService = require('./logout');
 const RegisterService = require('./register');
 
-function getServicesFromDataSource(persistentData) {
-    let authDAO = persistentData.authDAO;
-    let gameDAO = persistentData.gameDAO;
-    let gameRequestDAO = persistentData.gameRequestDAO;
-    let userDAO = persistentData.userDAO;
+module.exports = {
+    ClearApplicationService,
+    JoinGameService,
+    LoginService,
+    LogoutService,
+    RegisterService,
 
-    return {
-        clearApplicationService: new ClearApplicationService(authDAO, gameDAO, gameRequestDAO, userDAO),
-        joinGameService: new JoinGameService(authDAO, gameDAO, gameRequestDAO, userDAO),
-        loginService: new LoginService(authDAO, gameDAO, gameRequestDAO, userDAO),
-        logoutService: new LogoutService(authDAO, gameDAO, gameRequestDAO, userDAO),
-        registerService: new RegisterService(authDAO, gameDAO, gameRequestDAO, userDAO),
+    getServicesFromDataSource: function (persistentData) {
+        let authDAO = persistentData.authDAO;
+        let gameDAO = persistentData.gameDAO;
+        let gameRequestDAO = persistentData.gameRequestDAO;
+        let userDAO = persistentData.userDAO;
+
+        return {
+            clearApplicationService: new ClearApplicationService(authDAO, gameDAO, gameRequestDAO, userDAO),
+            joinGameService: new JoinGameService(authDAO, gameDAO, gameRequestDAO, userDAO),
+            loginService: new LoginService(authDAO, gameDAO, gameRequestDAO, userDAO),
+            logoutService: new LogoutService(authDAO, gameDAO, gameRequestDAO, userDAO),
+            registerService: new RegisterService(authDAO, gameDAO, gameRequestDAO, userDAO),
+        }
     }
-}
-
-module.exports.getServicesFromDataSource = getServicesFromDataSource;
-module.exports.ClearApplicationService = ClearApplicationService;
-module.exports.JoinGameService = JoinGameService;
-module.exports.LoginService = LoginService;
-module.exports.LogoutService = LogoutService;
-module.exports.RegisterService = RegisterService;
+};
