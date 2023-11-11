9e3db5f2239ecb10b826d580d9ff8f8fe771865b
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-11-10 21:14:19 -0700

    feat(test): implement gameDAO tests

--- test/dataAccess/gameDAO.test.js
+++ test/dataAccess/gameDAO.test.js
@@ -1,95 +1,182 @@
 'use strict';
 
-// insertNewGame negative test
-test('insert_game_twice_with_same_gameID_throws_error', (done) => {
-    throw new Error("Unimplemented test!"); // TODO test
+import {
+    GameDAO, UserDAO, NoSuchItemError, ValueAlreadyTakenError, BadRequestError
+} from '../../server/dataAccess/dataAccess';
+import {Game, User} from '../../server/models';
+import {Board, DEFAULT_BOARD_DIMENSIONS} from '../board';
+
+let gameDAO;
+let userDAO;
+const user1 = new User('user1', 'pass1');
+const user2 = new User('user2', 'pass2');
+
+const board1 = new Board(DEFAULT_BOARD_DIMENSIONS, null);
+const board2 = new Board(DEFAULT_BOARD_DIMENSIONS, null);
+board1.markTaken(0, 0);
+board2.markTaken(1, 1);
+
+const game1 = new Game(board1);
+const game2 = new Game(board2);
+game2.toggleTurn();
+
+beforeEach(() => {
+    gameDAO = new GameDAO();
+    userDAO = new UserDAO();
+    userDAO.insertNewUser(user1);
+    userDAO.insertNewUser(user2);
 });
 
 
-// insertNewGame positive test
-test('findReturnsGameWithEqualBoard', (done) => {
-    throw new Error("Unimplemented test!"); // TODO test
+// test the test
+test('board1 test variable and board2 test variable are not equal', () => {
+    expect(board1).not.toBe(board2);
+});
+
+// test the test
+test('board1 test variable is equal to itself', () => {
+    expect(board1).toBe(board1);
 });
 
 
-test('findReturnsGameWithSameGameID', (done) => {
-    throw new Error("Unimplemented test!"); // TODO test
+test('insert valid game does not error', () => {
+    expect(() => gameDAO.insertGame('user1', 'user2', game1)).not.toThrow();
 });
 
 
-test('findReturnsGameWithSameGameName', (done) => {
-    throw new Error("Unimplemented test!"); // TODO test
+test('insert game that already exists throws already taken error', () => {
+    gameDAO.insertGame('user1', 'user2', game1);
+    expect(() => gameDAO.insertGame('user1', 'user2', game1)).toThrow(ValueAlreadyTakenError);
 });
 
 
-test('findReturnsGameWithSamePlayers', (done) => {
-    throw new Error("Unimplemented test!"); // TODO test
+test('insert game with invalid first player throws bad request error', () => {
+    expect(() => gameDAO.insertGame('iDoNotExist', 'user2', game1))
+        .toThrow(BadRequestError);
 });
 
 
-test('findReturnsGameWithSameNumberOfSpectators', (done) => {
-    throw new Error("Unimplemented test!"); // TODO test
+test('insert game with invalid second player throws bad request error', () => {
+    expect(() => gameDAO.insertGame('user1', 'iDoNotExist', game1))
+        .toThrow(BadRequestError);
 });
 
 
-test('find game with players in the opposite order still returns equal board', () => {
-    // TODO test
+test('find inserted game returns game with equal board', () => {
+    gameDAO.insertGame('user1', 'user2', game1);
+    expect(gameDAO.findGame('user1', 'user2')).toBe(board1);
 });
 
 
-test('find game with players in the opposite order returns correct turn status', () => {
-    // TODO test
+test('find inserted game returns game with same turn status', () => {
+    gameDAO.insertGame('user1', 'user2', game1);
+    expect(gameDAO.findGame('user1', 'user2').isFirstPlayerTurn())
+        .toBe(game1.isFirstPlayerTurn());
 });
 
 
-// findGame negative test
-test('findNonexistentGameErrors', () => {
-    throw new Error("Unimplemented test!"); // TODO test
+test('find with players in the opposite order returns game with equal board', () => {
+    gameDAO.insertGame('user1', 'user2', game1);
+    expect(gameDAO.findGame('user2', 'user1')).toBe(board1);
 });
 
 
-// removeGame positive test
-test('findRemovedGameErrors', (done) => {
-    throw new Error("Unimplemented test!"); // TODO test
+test('find with players in the opposite order returns game with same turn status', () => {
+    gameDAO.insertGame('user1', 'user2', game1);
+    expect(gameDAO.findGame('user2', 'user1').isFirstPlayerTurn())
+        .toBe(game1.isFirstPlayerTurn());
 });
 
 
-// removeGame "negative" test
-test('removeNonexistentGameDoesNotThrow', (done) => {
-    throw new Error("Unimplemented test!"); // TODO test
+test('find nonexistent game throws no such item error', () => {
+    expect(() => gameDAO.findGame('user1', 'user2')).toThrow(NoSuchItemError);
 });
 
 
-// updateGameState positive test
-test('updateGameState_changes_board_state', (done) => {
-    throw new Error("Unimplemented test!"); // TODO test
+test('find removed game throws no such item error', () => {
+    gameDAO.insertGame('user1', 'user2', game1);
+    gameDAO.removeGame('user1', 'user2');
+    expect(() => gameDAO.findGame('user1', 'user2')).toThrow(NoSuchItemError);
 });
 
 
-// updateGameState negative test
-test('updateGameState_of_nonexistent_game_throws_error', (done) => {
-    throw new Error("Unimplemented test!"); // TODO test
+test('remove nonexistent game does not throw no such item error', () => {
+    expect(() => gameDAO.removeGame('user1', 'user2')).not.toThrow(NoSuchItemError);
 });
 
 
-test('updateGameState_of_removed_game_throws_error', () => {
-    throw new Error("Unimplemented test!"); // TODO test
+test('remove with invalid first player throws bad request error', () => {
+    expect(() => gameDAO.removeGame('iDoNotExist', 'user2')).toThrow(NoSuchItemError);
 });
 
 
-// clearGames positive test
-test('findClearedGamesErrors', (done) => {
-    throw new Error("Unimplemented test!"); // TODO test
+test('remove with invalid second player throws bad request error', () => {
+    expect(() => gameDAO.removeGame('user1', 'iDoNotExist')).toThrow(NoSuchItemError);
 });
 
 
-// clearGames "negative" test
-test('clearEmptyDoesNotThrow', (done) => {
-    throw new Error("Unimplemented test!"); // TODO test
+test('find after updateGameState returns game with updated board', () => {
+    gameDAO.insertGame('user1', 'user2', game1);
+    gameDAO.updateGameState('user1', 'user2', board2);
+    const fetchedBoard = gameDAO.findGame('user1', 'user2').getBoard();
+    expect(fetchedBoard).toBe(board2);
 });
 
 
-// generateNewGameID positive test
-test('generateTwoGameIDsReturnsDifferentValues', (done) => {
-    throw new Error("Unimplemented test!"); // TODO test
+test('find after updateGameState with toggleTurn=true returns game with opposite turn status', () => {
+    gameDAO.insertGame('user1', 'user2', game1);
+    gameDAO.updateGameState('user1', 'user2', board2, true);
+
+    const fetchedTurnStatus = gameDAO.findGame('user1', 'user2').isFirstPlayerTurn();
+    const expectedTurnStatus = !game1.isFirstPlayerTurn();
+
+    expect(fetchedTurnStatus).isFirstPlayerTurn().toBe(expectedTurnStatus);
+});
+
+
+test('find after updateGameState with toggleTurn=false returns game with same turn status', () => {
+    gameDAO.insertGame('user1', 'user2', game1);
+    gameDAO.updateGameState('user1', 'user2', board2, false);
+
+    const fetchedTurnStatus = gameDAO.findGame('user1', 'user2').isFirstPlayerTurn();
+    const expectedTurnStatus = game1.isFirstPlayerTurn();
+
+    expect(fetchedTurnStatus).isFirstPlayerTurn().toBe(expectedTurnStatus);
+});
+
+
+test('find after updateGameState without toggleTurn parameter returns game with same turn status', () => {
+    gameDAO.insertGame('user1', 'user2', game1);
+    gameDAO.updateGameState('user1', 'user2', board2);
+
+    const fetchedTurnStatus = gameDAO.findGame('user1', 'user2').isFirstPlayerTurn();
+    const expectedTurnStatus = game1.isFirstPlayerTurn();
+
+    expect(fetchedTurnStatus).isFirstPlayerTurn().toBe(expectedTurnStatus);
+});
+
+
+test('updateGameState of nonexistent game throws no such item error', () => {
+    expect(() => gameDAO.updateGameState('user1', 'user2', board2))
+        .toThrow(NoSuchItemError);
+});
+
+
+test('updateGameState of removed game throws no such item error', () => {
+    gameDAO.insertGame('user1', 'user2', game1);
+    gameDAO.removeGame('user1', 'user2');
+    expect(() => gameDAO.updateGameState('user1', 'user2', board2))
+        .toThrow(NoSuchItemError);
+});
+
+
+test('find cleared games throws no such item error', () => {
+    gameDAO.insertGame(game1);
+    gameDAO.clearGames();
+    expect(gameDAO.findGame('user1', 'user2')).toThrow(NoSuchItemError);
+});
+
+
+test('clear empty does not throw', () => {
+    expect(() => gameDAO.clearGames()).not.toThrow();
 });
