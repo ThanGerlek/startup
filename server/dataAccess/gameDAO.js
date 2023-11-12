debee7d8dd2b4a14e763745a23abe08b93c50c07
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-11-11 23:25:14 -0700

    feat(dao): begin implementing gameDAO

--- server/dataAccess/gameDAO.js
+++ server/dataAccess/gameDAO.js
@@ -3,7 +3,10 @@
 // Game Data Access
 // TODO! db
 
+const {NoSuchItemError, BadRequestError} = require("./DataAccessErrors");
+
 class GameDAO {
+    #gameList;
     #userDAO;
 
     constructor(userDAO) {
@@ -11,70 +14,122 @@ class GameDAO {
         this.#userDAO = userDAO;
     }
 
+    // TODO. replace BadRequestError for invalid usernames with a better error type
+    // TODO? extract invalid username checking to handler level?
+
     /**
      * Inserts a game into the database.
+     * @param player1 the username of one of the players (order doesn't matter)
+     * @param player2 the username of one of the players (order doesn't matter)
      * @param game the game to insert
+     * @throws ValueAlreadyTakenError if a game between the players already exists
+     * @throws BadRequestError if given an invalid username
      */
-    insertGame(game) {
-        /* Failures
-        can't access database
-        */
-        // TODO
+    insertGame(player1, player2, game) {
+        // Failures: game already exists, invalid usernames
+        this.#requirePlayers(player1, player2);
+        let gameListItem = new GameListItem(player1, player2, game);
+        this.#gameList.push(gameListItem);
     }
 
     /**
      * Fetches the game between the given players from the database.
      *
-     * @param firstPlayer the username of one of the players in the game (order does not matter)
-     * @param secondPlayer the username of the other player in the game (order does not matter)
+     * @param player1 the username of one of the players in the game (order does not matter)
+     * @param player2 the username of the other player in the game (order does not matter)
      * @return the fetched game
+     * @throws NoSuchItemError if the game is not found
+     * @throws BadRequestError if given an invalid username
      */
-    findGame(firstPlayer, secondPlayer) {
-        /* Failures
-        can't access database
-        game not found
-        */
-        // TODO
-        return null;
+    findGame(player1, player2) {
+        // Failures: game not found, invalid usernames
+        this.#requirePlayers(player1, player2);
+
+        const gameIndex = this.#requireGameIndex(player1, player2);
+        if (gameIndex < 0) {
+            throw new NoSuchItemError(`Game not found for players '${player1}' and '${player2}'`);
+        } else {
+            return this.#gameList[gameIndex].game;
+        }
+    }
+
+    #getGameIndex(player1, player2) {
+        for (let i = 0; i < this.#gameList.length; i++) {
+            if (this.#gameList[i].matches(player1, player2)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    #requireGameIndex(player1, player2) {
+        const index = this.#getGameIndex(player1, player2);
+        if (index < 0) {
+            throw new NoSuchItemError(`Game not found for players '${player1}' and '${player2}'`);
+        } else {
+            return index;
+        }
+    }
+
+    #requirePlayers(player1, player2) {
+        if (!this.#userDAO.hasUser(player1) || !this.#userDAO.hasUser(player2)) {
+            throw new BadRequestError('Failed to find game, invalid username was provided');
+        }
     }
 
     /**
      * Updates the game state of a game in the database to match the given version.
      *
-     * @param firstPlayer the username of one of the players in the game (order does not matter)
-     * @param secondPlayer the username of the other player in the game (order does not matter)
+     * @param player1 the username of one of the players in the game (order does not matter)
+     * @param player2 the username of the other player in the game (order does not matter)
      * @param game the updated version of the Game
+     * @throws NoSuchItemError if the game is not found
+     * @throws BadRequestError if given an invalid username
      */
-    updateGameState(firstPlayer, secondPlayer, game) {
-        /* Failures
-        can't access database
-        game not found
-        */
+    updateGameState(player1, player2, game) {
+        // Failures: game not found, invalid usernames
         // TODO
+        this.#requirePlayers(player1, player2);
+        const gameIndex = this.#requireGameIndex(player1, player2);
+        this.#gameList[gameIndex] = new GameListItem(player1, player2, game);
     }
 
     /**
      * Removes a single game from the database.
      *
-     * @param firstPlayer the username of one of the players in the game (order does not matter)
-     * @param secondPlayer the username of the other player in the game (order does not matter)
+     * @param player1 the username of one of the players in the game (order does not matter)
+     * @param player2 the username of the other player in the game (order does not matter)
+     * @throws BadRequestError if given an invalid username
      */
-    removeGame(firstPlayer, secondPlayer) {
-        /* Failures
-        can't access database
-        game not found
-        */
+    removeGame(player1, player2) {
+        // Failures: game not found, invalid usernames
         // TODO
+        this.#requirePlayers(player1, player2);
+        const gameIndex = this.#requireGameIndex(player1, player2);
+
     }
 
     /**
      * Removes every game from the database.
      */
     clearGames() {
-        /* Failures
-        can't access database
-        */
         // TODO
+    }
+}
+
+class GameListItem {
+    player1;
+    player2;
+    game;
+
+    constructor(player1, player2, game) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.game = game;
+    }
+
+    matches(player, otherPlayer) {
+        return (this.player1 === player && this.player2 === otherPlayer) || (this.player1 === otherPlayer && this.player2 === player);
     }
 }
 
