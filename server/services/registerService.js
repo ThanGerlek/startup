02e73ec477e57b31d49d0becd2a8961c7897eba3
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-11-13 21:59:31 -0700

    feat(service): initial registerService implementation

--- server/services/registerService.js
+++ server/services/registerService.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const {AuthResponse} = require('../http');
+const {ValueAlreadyTakenError} = require("../dataAccess/dataAccess");
+const {User} = require("../models");
 
 class RegisterService {
     #authDAO;
@@ -11,11 +13,29 @@ class RegisterService {
         this.#userDAO = userDAO;
     }
 
-    register() {
-        // TODO! services
+    register(authRequest) {
         console.log("Called register()");
 
-        return new AuthResponse("Registered user.", "1234");
+        const username = authRequest.username;
+        const password = authRequest.password;
+
+        if (this.#userDAO.hasUser(username)) {
+            throw new ValueAlreadyTakenError(`Failed to register, username ${username} is already taken`);
+        } else {
+            const user = new User(username, password);
+            this.#userDAO.insertNewUser(user);
+
+            const token = this.#generateToken();
+            this.#authDAO.addToken(token);
+
+            return new AuthResponse(`Registered new user successfully`, token, username);
+        }
+    }
+
+    // TODO remove duplicated code from loginService.js
+    #generateToken() {
+        // TODO! Replace with something cryptographically secure!
+        return JSON.stringify(Math.random() * 2749871491 + Math.random());
     }
 }
 
