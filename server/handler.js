59ef169a0c72b4a3c3acbd01b3596001def61260
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-11-14 09:41:49 -0700

    enhance(js): include error objects in HTTP error responses

--- server/handler.js
+++ server/handler.js
@@ -14,11 +14,11 @@ handleResponse = function (res, runService) {
         500 DataAccessError
         */
         if (e instanceof BadRequestError || e instanceof NoSuchItemError || e instanceof ValueAlreadyTakenError) {
-            res.send(400, new ErrorResponse(e.message));
+            res.send(400, new ErrorResponse(e.message, e));
         } else if (e instanceof UnauthorizedAccessError) {
-            res.send(403, new ErrorResponse(e.message));
+            res.send(403, new ErrorResponse(e.message, e));
         } else if (e instanceof DataAccessError) {
-            res.send(500, new ErrorResponse(e.message));
+            res.send(500, new ErrorResponse(e.message, e));
         } else {
             res.send(500, new ErrorResponse(`Unrecognized server error`, e))
         }
