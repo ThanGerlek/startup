54527fc72253104bedad7326a578ca3d9f25e04e
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-12-08 10:24:34 -0700

    feat: create gameData when starting local games

--- public/find-friend-local.js
+++ public/find-friend-local.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import {displayMessage} from "./message-display.js";
+import {DEFAULT_BOARD_DIMENSIONS} from "./board.mjs";
 
 function onSubmitButtonClick() {
     let playerUsername = localStorage.getItem('username');
@@ -11,9 +12,32 @@ function onSubmitButtonClick() {
     } else if (opponentUsername === playerUsername) {
         displayMessage('warn', "Woah! That's your friend's name! Please try something else.");
     } else {
-        localStorage.setItem('opponentUsername', opponentUsername);
+        localStorage.setItem('opponentUsername', opponentUsername); // TODO? Unneeded?
+        storeNewGame(playerUsername, opponentUsername);
         window.location.href = 'game.html';
     }
+}
+
+function storeNewGame(playerUsername, opponentUsername) {
+    const gameData = {
+        playerUsername: playerUsername,
+        opponentUsername: opponentUsername,
+        isPlayerTurn: true, // TODO allow switching who goes first
+        board: generateNewBoardArray(),
+    };
+    localStorage.setItem('game', JSON.stringify(gameData));
+}
+
+function generateNewBoardArray() {
+    const boardArray = [];
+    DEFAULT_BOARD_DIMENSIONS.forEach(rowSize => {
+        const row = [];
+        for (let i = 0; i < rowSize; i++) {
+            row.push(false);
+        }
+        boardArray.push(row);
+    });
+    return boardArray;
 }
 
 document.getElementById('submit-game-request-button').addEventListener('click', onSubmitButtonClick);
