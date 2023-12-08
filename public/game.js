3deea97515283dcd704ffcf12504255636093ac5
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-12-08 16:42:15 -0700

    feat: replace artificial data with websocket

--- public/game.js
+++ public/game.js
@@ -73,89 +73,9 @@ function playerWin() {
     window.location.href = 'you-win.html';
 }
 
-async function submitMoveToServer(gameData) {
-    if (gameData.gameType === 'local') {
-        return;
-    }
-
-    clearMessageDisplay();
-    setupWaitNotification(1000);
-
-    localStorage.setItem('game', JSON.stringify(gameData));
-
-    let username = localStorage.getItem('username');
-    let opponentUsername = localStorage.getItem('opponentUsername');
-
-    try {
-        let response = await getSubmitMoveResponse(gameData, username, opponentUsername);
-        cancelWaitNotification();
-        parseSubmitMoveResponse(response);
-    } catch (err) {
-        cancelWaitNotification();
-        let msg = `Failed to connect to the server. Make sure you're connected to the internet, or try again later.`;
-        displayMessage('error', msg);
-        // TODO. err: handle by ending game or trying to reconnect?
-    }
-}
-
-async function getSubmitMoveResponse(gameData, username, opponentUsername) {
-    // Return artificial data
-
-    return new Promise((resolve, reject) => {
-        console.log(`Simulating accessing server to submit move. User: '${username}', opponent: '${opponentUsername}'`);
-
-        let response = {};
-
-        if (username === opponentUsername) {
-            response = new ErrorResponse('invalidUser');
-        } else {
-            response = new OKResponse();
-        }
-
-
-        // TODO server: remove test code
-        if (username === "board_test-bad-response") {
-            console.log('Test: simulating receiving a malformed response');
-            response = {
-                value: 'blue', errorType: 'french fries', token: {tokenString: 'ur face', username: 'abraham lincoln'}
-            };
-        } else if (username === "board_invalid-but-logged-in") {
-            console.log('Test: simulating a "logged in" user with an invalid username (account removed during gameplay?)');
-            response = new ErrorResponse('invalidUser');
-        } else if (username === "board_test-server-access-failure") {
-            console.log('Test: simulating a server access failure');
-            reject();
-        }
-
-        setTimeout(() => resolve(response), 2000);
-        // resolve(response);
-    });
-}
-
-function parseSubmitMoveResponse(response) {
-    if (isOKResponse(response)) {
-        clearMessageDisplay();
-        changeTurn();
-    } else if (isInvalidUserResponse(response)) {
-        displayMessage('error', 'Woah! Invalid username! Try restarting the game.');
-        // TODO. err: handle by ending game?
-    } else {
-        displayMessage('error', 'Failed to parse HTTP response! Please try again.');
-    }
-}
-
-function changeTurn() {
-    console.log(`Changing turn`);
-    clientGame.changeTurn();
-    updateCurrentPlayerText();
-}
-
-function isInvalidUserResponse(response) {
-    return response.value === 'error' && response.errorType === 'invalidUser'; // temporary artificial implementation
-}
-
-function isOKResponse(response) {
-    return response.value === '200 OK'; // temporary artificial implementation
+function submitMoveToServer() {
+    const message = {'action': 'submitMove', 'value': clientGame.getGameData()};
+    socket.send(JSON.stringify(message));
 }
 
 function updateCurrentPlayerText() {
