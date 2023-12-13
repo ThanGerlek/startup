88105573ad2538196b2329015e5667d489df9a85
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-12-12 20:35:40 -0700

    feat: replace you-lose.mjs with you-lose.jsx

--- src/postgame/you-lose.jsx
+++ src/postgame/you-lose.jsx
@@ -2,21 +2,32 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 
 export function YouLose() {
-    return (
-        <main className="mx-3 flex-grow-1 d-flex flex-column justify-content-around align-items-center">
-            <div></div>
 
-            <h1 className="text-center" style={{fontSize: 50, fontStyle: "italic"}}>YOU LOST!!!</h1>
+    const [quote, setQuote] = React.useState(null);
 
-            <p className="text-center">You must be horribly ashamed. But don't worry, maybe you can redeem yourself and restore
-                honor to your family.</p>
+    React.useEffect(() => {
+        getQuote();
+    });
 
-            <NavLink className="btn btn-success" to='/home'>Play again!</NavLink>
+    async function getQuote() {
+        const quote = await fetch('https://api.quotable.io/random')
+            .then(response => response.json())
+            .then(jsonResponse => {
+                const text = jsonResponse.content;
+                const author = jsonResponse.author;
+                return `"${text}" (${author})`
+            });
+        setQuote(quote);
+    }
 
-            {/* TODO make this nicer-looking */}
-            <p className="text-center" id="quote">Bob</p>
-
-            <div></div>
-        </main>
-    );
+    return (<main className="mx-3 flex-grow-1 d-flex flex-column justify-content-around align-items-center">
+        <div></div>
+        <h1 className="text-center" style={{fontSize: 50, fontStyle: "italic"}}>YOU LOST!!!</h1>
+        <p className="text-center">You must be horribly ashamed. But don't worry, maybe you can redeem yourself and
+            restore honor to your family.</p>
+        <NavLink className="btn btn-success" to='/home'>Play again!</NavLink>
+        {/* TODO make this nicer-looking */}
+        <p className="text-center" id="quote">{quote}</p>
+        <div></div>
+    </main>);
 }
