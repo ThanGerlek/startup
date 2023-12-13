03d3570a34456150a731c97b0b5500ae5edc80bd
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-12-12 17:22:35 -0700

    enhance: collapse Secretses menu links

--- src/general/secretses.jsx
+++ src/general/secretses.jsx
@@ -2,30 +2,25 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 
 export function Secretses() {
-    return (
-        <main className="mx-3 flex-grow-1 d-flex flex-column justify-content-between align-items-center">
-            <div className="pageDescription">
-                <h3 className="pageTitle">You found the secretses!</h3>
-                <p>This is just a test page with links to everything. It doesn't have admin privileges or anything like
-                    that, it's just a shortcut.
-                </p>
-            </div>
+    const routeNames = ["game", "find-friend-remote", "find-friend-local", "home", "login", "stats", "wait-for-friend", "you-lose", "you-win"];
+    const routeLinks = routeNames.map(name => {
+        return <li><NavLink className='btn btn-link' to={'/' + name}>{name}</NavLink></li>;
+    });
 
-            <nav>
-                <menu>
-                    <NavLink className='nav-link' to='/game'>game</NavLink>
-                    <NavLink className='nav-link' to='/find-friend-remote'>find-friend-remote</NavLink>
-                    <NavLink className='nav-link' to='/find-friend-local'>find-friend-local</NavLink>
-                    <NavLink className='nav-link' to='/home'>home</NavLink>
-                    <NavLink className='nav-link' to='/login'>login</NavLink>
-                    <NavLink className='nav-link' to='/stats'>stats</NavLink>
-                    <NavLink className='nav-link' to='/wait-for-friend'>wait-for-friend</NavLink>
-                    <NavLink className='nav-link' to='/you-lose'>you-lose</NavLink>
-                    <NavLink className='nav-link' to='/you-win'>you-win</NavLink>
-                </menu>
-            </nav>
+    return (<main className="mx-3 flex-grow-1 d-flex flex-column justify-content-between align-items-center">
+        <div className="pageDescription">
+            <h3 className="pageTitle">You found the secretses!</h3>
+            <p>This is just a test page with links to everything. It doesn't have admin privileges or anything like
+                that, it's just a shortcut.
+            </p>
+        </div>
 
-            <div></div>
-        </main>
-    );
+        <nav>
+            <menu>
+                {routeLinks}
+            </menu>
+        </nav>
+
+        <div></div>
+    </main>);
 }
