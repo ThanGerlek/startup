67caa8c595497257658c6c83e485c5b25b6cb51c
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-12-13 23:19:14 -0700

    fix: update folder names

--- deployReact.sh
+++ deployReact.sh
@@ -24,10 +24,10 @@ mkdir build/services
 npm install # make sure vite is installed so that we can bundle
 npm run build # build the React front end
 cp -rf dist build/public # move the React front end to the target distribution
-cp service/*.js build # move the back end service to the target distribution
-cp service/*.json build
-cp service/dataAccess/* build/dataAccess
-cp service/services/* build/services
+cp server/*.js build # move the back end service to the target distribution
+cp server/*.json build
+cp server/dataAccess/* build/dataAccess
+cp server/services/* build/services
 
 # Step 2
 printf "\n----> Clearing out previous distribution on the target\n"
